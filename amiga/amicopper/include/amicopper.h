61a865585445e797db4170f53be7b39a9209b2d9
pos <pgousdal@users.noreply.github.com>
2026-09-24 18:10:19 +0200

    amiga: add Copper list storage and selection

--- amiga/amicopper/include/amicopper.h
+++ amiga/amicopper/include/amicopper.h
@@ -3,13 +3,13 @@
 
 #define AMICOPPER_NAME "AmiCopper"
 #define AMICOPPER_AREXX_PORT "AMICOPPER"
-#define AMICOPPER_API_VERSION 1
+#define AMICOPPER_API_VERSION 1\n#define AMICOPPER_MAX_INSTRUCTIONS 256
 
 struct AmiCopperDocument {
     const char *path;
     unsigned long instruction_count;
     int dirty;
-    int valid;
+    int valid;\n    unsigned short words[AMICOPPER_MAX_INSTRUCTIONS][2];\n    unsigned long selected;
 };
 
 void AmiCopperDocumentInit(struct AmiCopperDocument *doc);
