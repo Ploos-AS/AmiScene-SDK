bc8e3291f94a797850170795c2824cda864d472f
pos <pgousdal@users.noreply.github.com>
2026-09-24 18:03:46 +0200

    amiga: bind Copper model to native inspector

--- amiga/amicopper/include/amicopper_ui.h
+++ amiga/amicopper/include/amicopper_ui.h
@@ -1,13 +1,8 @@
 #ifndef AMISCENE_AMICOPPER_UI_H
 #define AMISCENE_AMICOPPER_UI_H
-
-struct AmiCopperUI {
-    void *window;
-    int running;
-};
-
+#include "amicopper_copper.h"
+struct AmiCopperUI { void *window; int running; struct AmiCopperInstruction selected; };
 int AmiCopperUIOpen(struct AmiCopperUI *ui);
 void AmiCopperUIClose(struct AmiCopperUI *ui);
 void AmiCopperUIRun(struct AmiCopperUI *ui);
-
 #endif
