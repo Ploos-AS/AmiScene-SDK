67900d4dc79dd3b675f9a7fe5ee2e264b216fc77
pos <pgousdal@users.noreply.github.com>
2026-09-24 20:33:37 +0200

    amiga: align native Copper validation with host rules

--- amiga/amicopper/src/document.c
+++ amiga/amicopper/src/document.c
@@ -5,7 +5,8 @@ void AmiCopperDocumentInit(struct AmiCopperDocument *doc)
     doc->path = 0;
     doc->instruction_count = 0;
     doc->dirty = 0;
-    doc->valid = 0;\n    doc->selected = 0;
+    doc->valid = 0;
+    doc->selected = 0;
 }
 
 void AmiCopperDocumentMarkDirty(struct AmiCopperDocument *doc)
@@ -39,14 +40,29 @@ int AmiCopperDocumentDelete(struct AmiCopperDocument *doc)
 int AmiCopperDocumentValidate(struct AmiCopperDocument *doc, unsigned long *bad_index)
 {
     unsigned long i;
-    int ended=0;
+    int ended=0, have_wait=0;
+    unsigned short last_v=0, last_h=0;
     for(i=0;i<doc->instruction_count;i++) {
         unsigned short a=doc->words[i][0], b=doc->words[i][1];
-        if(ended) { if(bad_index)*bad_index=i; AmiCopperDocumentValidated(doc,0); return 0; }
+        if(ended) goto bad;
         if(a==0xffff && b==0xfffe) { ended=1; continue; }
-        if(!(a&1)) { if(a&1) { if(bad_index)*bad_index=i; AmiCopperDocumentValidated(doc,0); return 0; } }
-        else if((a&0x00fe)&1) { if(bad_index)*bad_index=i; AmiCopperDocumentValidated(doc,0); return 0; }
+        if(!(a&1)) {
+            /* Copper MOVE destination is an even custom-register offset. */
+            if(a < 0x0080 || a > 0x01fe) goto bad;
+        } else {
+            unsigned short v=(a>>8)&0xff, h=a&0xfe;
+            /* WAIT/SKIP IR2 low bit distinguishes SKIP; compare masks use even H bits. */
+            if((b&0x00fe)>0x00fe) goto bad;
+            if(!(b&1)) {
+                if(have_wait && (v<last_v || (v==last_v && h<last_h))) goto bad;
+                last_v=v; last_h=h; have_wait=1;
+            }
+        }
     }
-    if(!ended) { if(bad_index)*bad_index=doc->instruction_count; AmiCopperDocumentValidated(doc,0); return 0; }
+    if(!ended) goto bad_end;
     AmiCopperDocumentValidated(doc,1); return 1;
+bad:
+    if(bad_index)*bad_index=i; AmiCopperDocumentValidated(doc,0); return 0;
+bad_end:
+    if(bad_index)*bad_index=doc->instruction_count; AmiCopperDocumentValidated(doc,0); return 0;
 }
