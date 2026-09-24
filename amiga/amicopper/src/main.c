7e01e05241b1d798a0ce851dc6bae2a09ee136fa
pos <pgousdal@users.noreply.github.com>
2026-09-24 20:21:03 +0200

    amiga: seed native Copper document

--- amiga/amicopper/src/main.c
+++ amiga/amicopper/src/main.c
@@ -7,6 +7,13 @@ int main(void)
     struct AmiCopperDocument doc;
     struct AmiCopperUI ui;
     AmiCopperDocumentInit(&doc);
+    doc.words[0][0]=0x2c01; doc.words[0][1]=0xfffe;
+    doc.words[1][0]=0x0180; doc.words[1][1]=0x0003;
+    doc.words[2][0]=0x5001; doc.words[2][1]=0xfffe;
+    doc.words[3][0]=0x0180; doc.words[3][1]=0x0006;
+    doc.words[4][0]=0xffff; doc.words[4][1]=0xfffe;
+    doc.instruction_count=5;
+    ui.document=&doc;
 
     if (!AmiCopperUIOpen(&ui)) {
         fputs("AmiCopper: unable to open Intuition window\n", stderr);
