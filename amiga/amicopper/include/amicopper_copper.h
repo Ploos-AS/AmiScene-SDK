43f535ef2f808cfab965c0a7cf3bde18f1a58dea
pos <pgousdal@users.noreply.github.com>
2026-09-24 18:03:14 +0200

    amiga: add native Copper instruction model

---
+++ amiga/amicopper/include/amicopper_copper.h
@@ -0,0 +1,15 @@
+#ifndef AMISCENE_AMICOPPER_COPPER_H
+#define AMISCENE_AMICOPPER_COPPER_H
+
+enum AmiCopperOpcode { AMICOPPER_MOVE, AMICOPPER_WAIT, AMICOPPER_SKIP, AMICOPPER_END };
+
+struct AmiCopperInstruction {
+    enum AmiCopperOpcode opcode;
+    unsigned short word0;
+    unsigned short word1;
+};
+
+const char *AmiCopperOpcodeName(enum AmiCopperOpcode opcode);
+int AmiCopperDecode(unsigned short word0, unsigned short word1, struct AmiCopperInstruction *out);
+
+#endif
