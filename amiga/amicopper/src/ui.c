860c16bd525e979ae0651af32aefa95e9197141b
pos <pgousdal@users.noreply.github.com>
2026-09-24 18:03:49 +0200

    amiga: show decoded Copper words in inspector

--- amiga/amicopper/src/ui.c
+++ amiga/amicopper/src/ui.c
@@ -5,7 +5,7 @@
 #include <intuition/intuition.h>
 #include <proto/exec.h>
 #include <proto/intuition.h>
-#include "amicopper_ui.h"
+#include "amicopper_ui.h"\n#include <stdio.h>\n#include <string.h>
 
 int AmiCopperUIOpen(struct AmiCopperUI *ui)
 {
@@ -44,14 +44,14 @@ void AmiCopperUIClose(struct AmiCopperUI *ui)
 void AmiCopperUIRun(struct AmiCopperUI *ui)
 {
     struct Window *w = (struct Window *)ui->window;
-    draw_shell(w);\n    while (ui->running) {
+    AmiCopperDecode(0x2c01, 0xfffe, &ui->selected);\n    draw_shell(w, &ui->selected);\n    while (ui->running) {
         struct IntuiMessage *msg;
         Wait(1UL << w->UserPort->mp_SigBit);
         while ((msg = (struct IntuiMessage *)GetMsg(w->UserPort)) != 0) {
             ULONG cls = msg->Class;
             UWORD code = msg->Code;
             ReplyMsg((struct Message *)msg);
-            if (cls == IDCMP_REFRESHWINDOW) {\n                BeginRefresh(w);\n                draw_shell(w);\n                EndRefresh(w, TRUE);\n            }\n            if (cls == IDCMP_CLOSEWINDOW)
+            if (cls == IDCMP_REFRESHWINDOW) {\n                BeginRefresh(w);\n                draw_shell(w, &ui->selected);\n                EndRefresh(w, TRUE);\n            }\n            if (cls == IDCMP_CLOSEWINDOW)
                 ui->running = 0;
             /* ESC is an intentional keyboard-first M0 exit path. */
             if (cls == IDCMP_RAWKEY && code == 0x45)
