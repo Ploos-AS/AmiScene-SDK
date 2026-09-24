2e1a28ea3f14d990e8638a25b1ff18b16521bc58
pos <pgousdal@users.noreply.github.com>
2026-09-25 00:36:01 +0200

    amiga: format Copper instructions as source

--- amiga/amicopper/src/copper.c
+++ amiga/amicopper/src/copper.c
@@ -1,4 +1,4 @@
-#include "amicopper_copper.h"
+#include "amicopper_copper.h"\n#include <stdio.h>
 
 const char *AmiCopperOpcodeName(enum AmiCopperOpcode opcode)
 {
@@ -19,5 +19,19 @@ int AmiCopperDecode(unsigned short w0, unsigned short w1, struct AmiCopperInstru
     else if (!(w0 & 1)) out->opcode = AMICOPPER_MOVE;
     else if (w1 & 1) out->opcode = AMICOPPER_SKIP;
     else out->opcode = AMICOPPER_WAIT;
+    return 1;
+}
+
+int AmiCopperFormatSource(const struct AmiCopperInstruction *ins, char *buf, unsigned int size)
+{
+    unsigned int v,h;
+    if(!ins||!buf||!size)return 0;
+    switch(ins->opcode){
+    case AMICOPPER_MOVE: snprintf(buf,size,"MOVE $%03x,$%04x",ins->word0,ins->word1); break;
+    case AMICOPPER_WAIT: v=ins->word0>>8; h=ins->word0&0xfe; snprintf(buf,size,"WAIT %u,%u",v,h); break;
+    case AMICOPPER_SKIP: v=ins->word0>>8; h=ins->word0&0xfe; snprintf(buf,size,"SKIP %u,%u",v,h); break;
+    case AMICOPPER_END: snprintf(buf,size,"END"); break;
+    default:return 0;
+    }
     return 1;
 }
