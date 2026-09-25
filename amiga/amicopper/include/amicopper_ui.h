7d381c322aa3c09176568a9ab2a8152fb1a39dc4
pos <pgousdal@users.noreply.github.com>
2026-09-25 02:30:12 +0200

    amiga: add document path state to UI

--- amiga/amicopper/include/amicopper_ui.h
+++ amiga/amicopper/include/amicopper_ui.h
@@ -1,7 +1,7 @@
 #ifndef AMISCENE_AMICOPPER_UI_H
 #define AMISCENE_AMICOPPER_UI_H
 #include "amicopper_copper.h"\n#include "amicopper.h"
-struct AmiCopperUI { void *window; int running; struct AmiCopperInstruction selected; struct AmiCopperDocument *document; char edit[96]; unsigned int edit_len; int editing; };
+struct AmiCopperUI { void *window; int running; struct AmiCopperInstruction selected; struct AmiCopperDocument *document; char edit[96]; unsigned int edit_len; int editing; char path[256]; };
 int AmiCopperUIOpen(struct AmiCopperUI *ui);
 void AmiCopperUIClose(struct AmiCopperUI *ui);
 void AmiCopperUIRun(struct AmiCopperUI *ui);
