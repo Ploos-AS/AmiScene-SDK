c6c4d7debf9337f9ee26d0ef7b6dc12357db95aa
pos <pgousdal@users.noreply.github.com>
2026-09-25 13:07:55 +0200

    amiga: own AmiCopper document path storage

--- amiga/amicopper/include/amicopper.h
+++ amiga/amicopper/include/amicopper.h
@@ -3,14 +3,17 @@
 
 #define AMICOPPER_NAME "AmiCopper"
 #define AMICOPPER_AREXX_PORT "AMICOPPER"
-#define AMICOPPER_API_VERSION 1\n#define AMICOPPER_MAX_INSTRUCTIONS 256
+#define AMICOPPER_API_VERSION 1
+#define AMICOPPER_MAX_INSTRUCTIONS 256
 #define AMICOPPER_AREXX_RESULT_MAX 160
 
 struct AmiCopperDocument {
-    const char *path;
+    char path[256];
     unsigned long instruction_count;
     int dirty;
-    int valid;\n    unsigned short words[AMICOPPER_MAX_INSTRUCTIONS][2];\n    unsigned long selected;
+    int valid;
+    unsigned short words[AMICOPPER_MAX_INSTRUCTIONS][2];
+    unsigned long selected;
 };
 
 void AmiCopperDocumentInit(struct AmiCopperDocument *doc);
